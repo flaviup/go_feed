8aa53a4eac43ef22782273f7d3c5638f78bd0b49
Flaviu Pasca <flaviup@gmail.com>
2019-08-05 23:26:36 +0300

    Added location picker for Android.

--- android/app/src/main/kotlin/com/go/go_feed/MainActivity.kt
+++ android/app/src/main/kotlin/com/go/go_feed/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugins.LocationPickerPlugin
 
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+
+    LocationPickerPlugin.registerWith(this.registrarFor("io.flutter.plugins.location_picker_plugin"))
   }
 }
